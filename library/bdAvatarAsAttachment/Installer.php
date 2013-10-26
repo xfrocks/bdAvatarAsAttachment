6ba61b69bfd0c6e79d4cd4129d0a9fe3f2daebaa
Dao Hoang Son <daohoangson@gmail.com>
2013-10-26 11:25:27 +0700

    added uninstall code to delete avatar attachments

--- library/bdAvatarAsAttachment/Installer.php
+++ library/bdAvatarAsAttachment/Installer.php
@@ -26,6 +26,55 @@ class bdAvatarAsAttachment_Installer
 
 		$db->delete('xf_content_type', array('addon_id = ?' => 'bdAvatarAsAttachment'));
 		$db->delete('xf_content_type_field', array('content_type = ?' => 'bdavatarasattachment_user'));
+
+		while (true)
+		{
+			$users = $db->fetchAll('
+				SELECT user_id
+				FROM `xf_user`
+				WHERE avatar_date = 1
+				LIMIT 100;
+			');
+
+			if (empty($users))
+			{
+				break;
+			}
+
+			$userIds = array();
+			$attachmentIds = array();
+			$dataIds = array();
+
+			foreach ($users as $user)
+			{
+				$userIds[] = $user['user_id'];
+
+				$attachments = $db->fetchAll('
+					SELECT attachment_id, data_id
+					FROM `xf_attachment`
+					WHERE content_type = ?
+						AND content_id = ?
+				', array(
+					'bdavatarasattachment_user',
+					$user['user_id']
+				));
+
+				foreach ($attachments as $attachment)
+				{
+					$attachmentIds[] = $attachment['attachment_id'];
+					$dataIds[] = $attachment['data_id'];
+				}
+			}
+
+			$db->update('xf_user', array(
+				'avatar_date' => 0,
+				'avatar_width' => 0,
+				'avatar_height' => 0,
+				'gravatar' => ''
+			), 'user_id IN (' . $db->quote($userIds) . ')');
+			$db->delete('xf_attachment', 'attachment_id IN (' . $db->quote($attachmentIds) . ')');
+			$db->update('xf_attachment_data', array('attach_count' => 0), 'data_id IN (' . $db->quote($dataIds) . ')');
+		}
 	}
 
 }
